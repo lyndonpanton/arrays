5b954eead5a9e93d63dc11e7d9283153eda5838c
Lyndon Panton <LyndonPanton@gmail.com>
2023-01-15 02:44:42 +0000

    Completed problem 2
    Took card at top of deck
    Added card to the card array
    Flipped over the card in the card array
    Output the card in the card array

--- Exercise11/Exercise11/Program.cs
+++ Exercise11/Exercise11/Program.cs
@@ -14,10 +14,19 @@ namespace Exercise11
         /// <param name="args">command-line args</param>
         static void Main(string[] args)
         {
+            // problem 1
             Deck deck = new Deck();
             Card[] cards = new Card[5];
 
             deck.Shuffle();
+
+            // problem 2
+            Card topCardOfDeck = deck.TakeTopCard();
+
+            cards[0] = topCardOfDeck;
+            cards[0].FlipOver();
+
+            Console.WriteLine(cards[0]);
         }
     }
 }
